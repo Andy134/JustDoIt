824b9a38aea79c56a907f7b608d017836d2359af
Luu Tuan Anh <anhlt@mintpotvn.com>
2021-03-08 12:44:22 +0700

    12

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+const App = () => {
+    return (
+        <div><h1>APP</h1></div>
+    )
 }
 
-export default App;
+export default App
---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv/config');
+const app = express();
+
+app.use(bodyParser.json())
+
+// ROUTES
+const postsRoute = require('./routes/posts');
+app.use('/posts', postsRoute);
+
+app.get('/', (req, res) => {
+    res.send(`Homepage`)
+})
+
+// connect toDB
+mongoose.connect(
+    process.env.DB_CONNECTION
+    , {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false,}
+    , () => console.log('connect to DB!'))
+
+    const PORT = 5000;
+    app.listen(PORT, function() {
+      console.log("Server is running on Port: " + PORT);
+    });
